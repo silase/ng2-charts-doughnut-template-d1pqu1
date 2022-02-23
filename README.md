5ebe65468af4bba7c377e82d5478c8147e989546
silase <silaseunice1994@gmail.com>
2022-02-23 19:37:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts-doughnut-template-6wxfhg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ng2-charts-doughnut-template-6wxfhg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
